84d47716bebcf4116138fc7e79dbf5a4fb8cb734
Sandyoho <Sandyoho@DESKTOP-3FM6O8C>
2023-04-27 15:40:31 +0200

    lite lattjo sattsej !

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("Jahaja har kan man ha kul!");
+        System.out.println();
+        
+        int k = 10;
+        
+        for (int i = 0; i < k; i++) 
+        {
+        	if(i < k/2)
+        		System.out.print(i + "\t");
+        	else if(i == k/2) 
+        	{
+        		System.out.println();
+        		System.out.print (i + "\t");
+        	}
+        	else if(i > k/2)
+        		System.out.print(i + "\t");
+		}
+        System.out.println("\n");
+        System.out.println("tada!~~");
     }
 }
