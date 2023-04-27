c1ebe4077337f530b3e81e17ca067ace759e7dfe
David Qvarnström <davidqvarnstrom@gmail.com>
2023-04-27 14:29:32 +0200

    Change name on class
    Changed the names of claasses and ditten och datten

---
+++ src/GameView.java
@@ -0,0 +1,19 @@
+public class GameView {
+
+    private GameModel gameModel;
+    
+    public GameView(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+
+    public void displayGameBoard() {
+        System.out.println("Hello!");        
+    }
+
+    public static void main(String[] args) {
+        GameModel gameModel = new GameModel(0, 1);
+        GameView gameUI = new GameView(gameModel);
+        gameUI.displayGameBoard();
+    }
+
+}
